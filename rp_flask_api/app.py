91ad3d92fa959f8d3414c5a514b4397f9f4c156b
Matty <matthew.j.percival@gmail.com>
2022-11-17 12:30:08 +0000

    return data frrom people endpoint

--- rp_flask_api/app.py
+++ rp_flask_api/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+# app .py
 
-app = Flask(__name__)
+from flask import render_template
+import connexion
+
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
 
 @app.route("/")
 def home():
